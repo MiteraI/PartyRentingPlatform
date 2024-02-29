51c534f66428e79fca61c97c3e84ffccc0ad249f
MiteraI <91677179+MiteraI@users.noreply.github.com>
2024-02-29 22:00:44 +0700

    Add view service details

---
+++ src/PartyRentingPlatform/Controllers/VnpayController.cs
@@ -0,0 +1,29 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyRentingPlatform.Domain.Services.Interfaces;
+
+namespace PartyRentingPlatform.Controllers
+{
+    [Route("api/vnpay")]
+    [ApiController]
+    public class VnpayController : ControllerBase
+    {
+        private readonly ILogger<VnpayController> _logger;
+        private readonly IVnpayService _vnpayService;
+
+        public VnpayController(ILogger<VnpayController> logger ,IVnpayService vnpayService)
+        {
+            _logger = logger;
+            _vnpayService = vnpayService;
+        }
+
+        [HttpGet("payment")]
+        public IActionResult GetPaymentUrl(double price)
+        {
+            return Ok(_vnpayService.CreateVnpayPortalUrl(price, Request.Headers["Referer"], HttpContext));
+        }
+    }
+}
